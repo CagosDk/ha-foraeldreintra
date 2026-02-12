10451e80b45dce382b979f041e9d3ec748a4b4a6
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-02-12 17:28:12 +0100

    Update __init__.py

--- custom_components/foraeldreintra/__init__.py
+++ custom_components/foraeldreintra/__init__.py
@@ -1,5 +1,9 @@
+from homeassistant.helpers.discovery import async_load_platform
+
 DOMAIN = "foraeldreintra"
 
 async def async_setup(hass, config):
+    hass.async_create_task(
+        async_load_platform(hass, "sensor", DOMAIN, {}, config)
+    )
     return True
-
