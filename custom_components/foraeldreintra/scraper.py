bc1c61e3932a351d3967268aec58049e5757a23a
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-02-12 18:04:34 +0100

    Create scraper.py

---
+++ custom_components/foraeldreintra/scraper.py
@@ -0,0 +1,8 @@
+import requests
+
+def hent_lektier(username, password, school_url):
+    # Midlertidigt mock – her indsætter vi din rigtige scraper bagefter
+    return {
+        "Frederik": "6 lektier",
+        "Olivia": "Ingen lektier"
+    }
