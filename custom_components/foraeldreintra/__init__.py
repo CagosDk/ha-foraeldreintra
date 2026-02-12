32cc3ada1ba16d7014c12b38e3c29153567f0e95
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-02-12 18:05:01 +0100

    Refactor async_setup to include configuration handling
    Store configuration values for username, password, and school_url in hass.data.

--- custom_components/foraeldreintra/__init__.py
+++ custom_components/foraeldreintra/__init__.py
@@ -1,5 +1,18 @@
 DOMAIN = "foraeldreintra"
 
 async def async_setup(hass, config):
-    hass.data.setdefault(DOMAIN, {})
+    conf = config.get(DOMAIN)
+
+    hass.data[DOMAIN] = {
+        "username": conf.get("username"),
+        "password": conf.get("password"),
+        "school_url": conf.get("school_url"),
+    }
+
+    hass.async_create_task(
+        hass.helpers.discovery.async_load_platform(
+            "sensor", DOMAIN, {}, config
+        )
+    )
+
     return True
