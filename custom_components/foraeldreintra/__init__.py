b9f5090e1cf0a2c716af8678efdadd271af94c03
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-02-12 17:58:03 +0100

    Update __init__.py

--- custom_components/foraeldreintra/__init__.py
+++ custom_components/foraeldreintra/__init__.py
@@ -1,11 +1,5 @@
-from homeassistant.const import Platform
-
 DOMAIN = "foraeldreintra"
-PLATFORMS = [Platform.SENSOR]
 
 async def async_setup(hass, config):
-    return True
-
-async def async_setup_entry(hass, entry):
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    hass.data.setdefault(DOMAIN, {})
     return True
