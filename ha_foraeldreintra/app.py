2de289638c9ac0f0dd2d8336625f23f284d92b4a
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-02-13 16:54:58 +0100

    Update app.py

--- ha_foraeldreintra/app.py
+++ ha_foraeldreintra/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route("/lektier")
+def lektier():
+    return jsonify({
+        "status": "running",
+        "message": "Addon works"
+    })
+
 @app.route("/")
-def hello():
-    return "Addon is running!"
+def index():
+    return "ForældreIntra Scraper is running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
