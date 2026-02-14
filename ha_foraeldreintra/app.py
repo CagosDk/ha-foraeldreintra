437f21ba82a7e6dad244078ecd1c2fc0796d5e23
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-02-14 12:33:04 +0100

    Update app.py

--- ha_foraeldreintra/app.py
+++ ha_foraeldreintra/app.py
@@ -1,3 +1,4 @@
+print("=== STARTING FLASK ===")
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -14,4 +15,5 @@ def index():
     return "ForældreIntra Scraper is running"
 
 if __name__ == "__main__":
+    print("=== STARTING FLASK ===")
     app.run(host="0.0.0.0", port=8080, debug=False)
