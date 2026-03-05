7f440943800ff85314e59a24d8e07c80895241d1
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-03-05 16:11:26 +0100

    Update const.py

--- custom_components/foraeldreintra/const.py
+++ custom_components/foraeldreintra/const.py
@@ -4,6 +4,18 @@ CONF_SCHOOL_URL = "school_url"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 
-DEFAULT_SCAN_INTERVAL_MINUTES = 15  # var 5
-
 PLATFORMS = ["sensor"]
+
+# Options
+OPT_SELECTED_CHILDREN = "selected_children"        # list[str]
+OPT_INCLUDE_HISTORY = "include_history"            # bool
+
+OPT_SCAN_MODE = "scan_mode"                        # "interval" | "fixed_times"
+OPT_SCAN_INTERVAL_MINUTES = "scan_interval_minutes"  # int
+OPT_SCAN_TIMES = "scan_times"                      # CSV "06:30,12:00"
+
+DEFAULT_INCLUDE_HISTORY = True
+
+DEFAULT_SCAN_MODE = "interval"
+DEFAULT_SCAN_INTERVAL_MINUTES = 15
+DEFAULT_SCAN_TIMES = ""
